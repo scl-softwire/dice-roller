34dc4658e575facaf771a6129c5ecf5402bcea3e
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-03-06 11:05:26 +0000

    Part 1

--- src/com/training/Main.java
+++ src/com/training/Main.java
@@ -1,8 +1,16 @@
 package com.training;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to the dice roller!");
+        System.out.println("===========================");
+
+        Random rng = new Random();
+        int diceRoll = rng.nextInt(6) + 1;
+
+        System.out.println("You rolled: " + diceRoll);
     }
 }
