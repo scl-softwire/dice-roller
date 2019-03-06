40cdb8afcc752761e3c769c7a98a46a31f9a8cf6
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-03-06 11:33:18 +0000

    Part 3

--- src/com/training/Main.java
+++ src/com/training/Main.java
@@ -9,16 +9,21 @@ public class Main {
         System.out.println("Welcome to the dice roller!");
         System.out.println("===========================");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many dice would you like to roll?");
-        System.out.print("> ");
-        int diceToRoll = scanner.nextInt();
-        System.out.println();
+        int sizeOfDice = getIntegerFromUser("What size of dice would you like to roll?");
+        int diceToRoll = getIntegerFromUser("How many dice would you like to roll?");
 
         for (int roll = 0; roll < diceToRoll; roll++) {
             Random rng = new Random();
-            int diceRoll = rng.nextInt(6) + 1;
+            int diceRoll = rng.nextInt(sizeOfDice) + 1;
             System.out.println("You rolled: " + diceRoll);
         }
+    }
+
+    private static int getIntegerFromUser(String prompt) {
+        System.out.println(prompt);
+        System.out.print("> ");
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
